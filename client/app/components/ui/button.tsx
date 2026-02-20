f4ee0d5625bcd0a17e0b030cb49ca16d79a45f7e
Kalebe Nascimento <kalebe.nascimento@dbserver.com.br>
2026-02-20 13:04:51 -0300

    feat: refatora criação de pauta

--- client/app/components/ui/button.tsx
+++ client/app/components/ui/button.tsx
@@ -1,9 +1,16 @@
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: keyof typeof buttonVariant
+}
 
-export function Button({ children, ...props }: ButtonProps) {
+const buttonVariant = {
+  primary: "bg-primary-2 hover:bg-primary-1 text-base-5 font-bold rounded-md px-4 py-3",
+  secondary: "border-2 border-primary-2 hover-primary-1 text-primary-2 hover:text-primary-1 font-bold rounded-md px-4 py-3"
+}
+
+export function Button({ children, variant = "primary", ...props }: ButtonProps) {
   return (
     <button
-      className="border-2 border-primary-2 hover-primary-1 text-primary-2 hover:text-primary-1 font-bold rounded-md px-4 py-3"
+      className={buttonVariant[variant]}
       {...props}
     >
       {children}
